8259ed96bfffb5046fe11f5dc9f02791bdfa2bc7
mexal17 <mexal17@tut.by>
2018-11-17 18:37:14 +0300

    First commit

---
+++ Lecture-8/Task2/l8task2/Application.java
@@ -0,0 +1,103 @@
+package l8task2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Application {
+
+    private final static String INPUT_FILE_NAME = "c:/Work folder/Automation/Lesson#8/StudentsList.txt";
+    private final static String OUTPUT_FILE_NAME = "c:/Work folder/Automation/Lesson#8/OUT_PUT_StudentsList.txt";
+
+    private static List<Student> getListStudentsFromFile(String fileName) {
+        List<Student> students = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String textLine;
+            while ((textLine = bufferedReader.readLine()) != null) {
+                String[] row = textLine.split(" ");
+                students.add(new Student(students.size() + 1, row[0], row[1], Integer.valueOf(row[2])));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: file is not found. " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error during file reading. " + e.getMessage());
+        }
+        return students;
+    }
+
+    private static void printListIntoFile(String fileName, List<Student> students, String typeOperation) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(new File(fileName), true));
+            bufferedWriter.write(typeOperation + "\n");
+            for (Student student : students) {
+                bufferedWriter.write(student.toString() + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: file is not found. " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error during file writing. " + e.getMessage());
+        }
+        try {
+            if (bufferedWriter != null)
+                bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error during file closing. " + e.getMessage());
+        }
+    }
+
+    private static void printMapIntoFile(String fileName, Map<Integer, Student> studentMap, String typeOperation) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(new File(fileName), true));
+            bufferedWriter.write(typeOperation + "\n");
+            for (Map.Entry<Integer, Student> entryStudent : studentMap.entrySet()) {
+                bufferedWriter.write(entryStudent.toString() + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: file is not found. " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error during file writing. " + e.getMessage());
+        }
+        try {
+            if (bufferedWriter != null)
+                bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error during file closing. " + e.getMessage());
+        }
+    }
+
+    private static void showStudents(List<Student> students) {
+        students.stream().forEach(System.out::println);
+    }
+
+    private static List<Student> getSortedStudentsByAge(List<Student> students) {
+        return students.stream().sorted(Comparator.comparingInt(Student::getAge)).collect(Collectors.toList());
+    }
+
+    private static List<Student> getSortedStudentsByLastName(List<Student> students) {
+        return students.stream().sorted(Comparator.comparing(Student::getLastName)).collect(Collectors.toList());
+    }
+
+    private static Map<Integer, Student> getMapOfStudents(List<Student> students) {
+        return students.stream().collect(Collectors.toMap(Student::getId, student -> student));
+    }
+
+    private static Map<Integer, Student> getStudentsWithOlderAge(Map<Integer, Student> studentMap, int age) {
+        return studentMap.entrySet().stream().filter(student -> student.getValue().getAge() > age).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = getListStudentsFromFile(INPUT_FILE_NAME);
+        showStudents(students);
+        printListIntoFile(OUTPUT_FILE_NAME, getSortedStudentsByAge(students), "SortedStudentsByAge:");
+        printListIntoFile(OUTPUT_FILE_NAME, getSortedStudentsByLastName(students), "SortedStudentsByLastName:");
+        printMapIntoFile(OUTPUT_FILE_NAME, getMapOfStudents(students), "MapOfStudents:");
+        printMapIntoFile(OUTPUT_FILE_NAME, getStudentsWithOlderAge(getMapOfStudents(students), 30), "StudentsWithOlderAge > 30:");
+    }
+}
