de91d20609516a1f076493840d2b786eb7f36316
mexal17 <mexal17@tut.by>
2018-12-12 21:32:30 +0300

    Code improvements after mentor's check

--- Lecture-6/Task4/l6task4/Application.java
+++ Lecture-6/Task4/l6task4/Application.java
@@ -9,7 +9,13 @@ import java.util.function.Consumer;
 
 public class Application {
 
+    private static final int INDEX = 5000000;
+
     private static SimpleDateFormat formatForDate = new SimpleDateFormat("hh:mm:ss:SSS");
+
+    private static double getMathRandom(){
+        return Math.random();
+    }
 
     private static List<Double> getFilledList(List<Double> list){
         int counter = 0;
@@ -38,9 +44,9 @@ public class Application {
         List<Double> arrayList = getFilledList(new ArrayList<>());
         List<Double> linkedList = getFilledList(new LinkedList<>());
         System.out.println("---> Adding to ArrayList 1000 elements:");
-        makeListOperation(arrayList,list -> list.add(5000000, Math.random()));
+        makeListOperation(arrayList,list -> list.add(INDEX, getMathRandom()));
         System.out.println("---> Adding to LinkedList 1000 elements:");
-        makeListOperation(linkedList,list -> list.add(5000000, Math.random()));
+        makeListOperation(linkedList,list -> list.add(INDEX, getMathRandom()));
         System.out.println("\n---> Deleting from ArrayList 1000 elements:");
         makeListOperation(arrayList,list -> list.remove(Math.random()));
         System.out.println("---> Deleting from LinkedList 1000 elements:");
