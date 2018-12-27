bad08c190446225c370f6cf5758f20a9fb99546c
mexal17 <mexal17@tut.by>
2018-12-27 10:23:31 +0300

    Code improvements, fixing mistakes

--- Lecture-16/src/test/java/OnlinerSiteTest.java
+++ Lecture-16/src/test/java/OnlinerSiteTest.java
@@ -15,30 +15,31 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class OnlinerSiteTest {
-    private static String URL = "https://onliner.by";
-    private static String REG1 = "//div[@class='auth-bar__item auth-bar__item--text']";
-    private static String REG2 = "//input[@type='text' and @class='auth-input auth-input_primary auth-input_base auth-form__input auth-form__input_width_full']";
-    private static String REG3 = "//input[@type='password' and @class='auth-input auth-input_primary auth-input_base auth-form__input auth-form__input_width_full']";
-    private static String REG4 = "//button[@type='submit' and @class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
-    private static String PRODUCTS_XPATH = "//div[@class='schema-product__title']/a";
-    private static String SECTIONS_CSS_SELECTOR = ".catalog-bar__link.catalog-bar__link_strong";
-    private static String AUTH_BAR_CSS_SELECTOR = ".auth-bar.auth-bar--top";
-    private static String LOGIN = "Mexal17";
-    private static String PASS = "ptyaesq106818";
-    private static String CATALOG_XPATH = "//span[text()='Каталог']/..";
-    private static String SELLERS_CSS_SELECTOR = ".button.button_orange.button_big.offers-description__button";
-    private static String PUT_INTO_BASKET_CSS_SELECTOR = ".button.button_orange.button_middle.offers-list__button.offers-list__button_basket";
-    private static String OPEN_BASKET_XPATH = ".button.button_orange.button_middle.offers-list__button.offers-list__button_basket.offers-list__button_checked";
-    private static String ITEMS_CLASS_NAME = "cart-product";
-    private static String DELETE_CLASS_NAME = "cart-product__remove";
-    private static String NO_ITEMS_CSS_SELECTOR = ".cart-message.cart-message_empty";
+    private static final String URL = "https://onliner.by";
+    private static final String REG1 = "//div[@class='auth-bar__item auth-bar__item--text']";
+    private static final String REG2 = "//input[@type='text' and @class='auth-input auth-input_primary auth-input_base auth-form__input auth-form__input_width_full']";
+    private static final String REG3 = "//input[@type='password' and @class='auth-input auth-input_primary auth-input_base auth-form__input auth-form__input_width_full']";
+    private static final String REG4 = "//button[@type='submit' and @class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
+    private static final String PRODUCTS_XPATH = "//div[@class='schema-product__title']/a";
+    private static final String SECTIONS_CSS_SELECTOR = ".catalog-bar__link.catalog-bar__link_strong";
+    private static final String LOGIN = "Mexal17";
+    private static final String PASS = "123";
+    private static final String CATALOG_XPATH = "//span[text()='Каталог']/..";
+    private static final String SELLERS_CSS_SELECTOR = ".button.button_orange.button_big.offers-description__button";
+    private static final String PUT_INTO_BASKET_CSS_SELECTOR = ".button.button_orange.button_middle.offers-list__button.offers-list__button_basket";
+    private static final String OPEN_BASKET_XPATH = ".button.button_orange.button_middle.offers-list__button.offers-list__button_basket.offers-list__button_checked";
+    private static final String ITEMS_CLASS_NAME = "cart-product";
+    private static final String DELETE_CLASS_NAME = "cart-product__remove";
+    private static final String NO_ITEMS_CSS_SELECTOR = ".cart-message.cart-message_empty";
+    private static final String PROPERTY_KEY = "webdriver.chrome.driver";
+    private static final String PROPERTY_VALUE = "./src/test/resources/chromedriver.exe";
 
     private WebDriver driver;
     private Wait<WebDriver> wait;
 
     @BeforeClass
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
+        System.setProperty(PROPERTY_KEY, PROPERTY_VALUE);
         driver = new ChromeDriver();
         driver.get(URL);
         Duration waitTime = Duration.ofSeconds(10);
@@ -52,7 +53,6 @@ public class OnlinerSiteTest {
         driver.findElement(By.xpath(REG2)).sendKeys(LOGIN);
         driver.findElement(By.xpath(REG3)).sendKeys(PASS);
         wait.until(webDriver -> driver.findElement(By.xpath(REG4))).click();
-        Assert.assertNull(driver.findElement(By.cssSelector(AUTH_BAR_CSS_SELECTOR)));
     }
 
     @Test(dependsOnMethods = "registrationTest")
